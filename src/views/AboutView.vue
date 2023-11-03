3e7a4d281f54b0346a9e3f085a84a4eaf4e57bdb
尚泽 <shang_ze@thtf.com.cn>
2023-11-03 16:35:58 +0800

    feat 添加文本

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,27 +2,28 @@
  * @Author: [shangze]
  * @Date: 2023-09-22 09:30:31
  * @LastEditors: [shangze]
- * @LastEditTime: 2023-09-22 11:00:46
+ * @LastEditTime: 2023-11-03 16:35:34
  * @Description: 
 -->
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page1</h1>
+    <h1>This is an about page2</h1>
+    <h1>This is an about page3</h1>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue } from 'vue-property-decorator';
 
 @Component({
-  components: {
-  },
+  components: {},
 })
 export default class HomeView extends Vue {
   data() {
-   return {
-     message: 'Hello Vue.js!'
-   };
- }
+    return {
+      message: 'Hello Vue.js!',
+    };
+  }
 }
 </script>
