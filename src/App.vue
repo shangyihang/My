3a84345372a4c67a5e651784e189d518db84b56a
尚泽 <shang_ze@thtf.com.cn>
2023-11-03 11:30:22 +0800

    app页新增样式

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,10 @@
+<!--
+ * @Author: [shangze]
+ * @Date: 2023-10-23 18:10:12
+ * @LastEditors: [shangze]
+ * @LastEditTime: 2023-11-03 11:29:49
+ * @Description: 
+-->
 <template>
   <div id="app">
     <nav>
@@ -7,3 +14,26 @@
     <router-view />
   </div>
 </template>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
